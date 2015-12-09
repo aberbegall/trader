a0cfff530c2fc17729bcbfa1ce74d40ff44a1efb
Agustin Berbegall <aberbegall@yahoo.es>
2015-12-10 00:17:37 +0100

    Added all_test file and updated travis script

--- tool/travis.sh
+++ tool/travis.sh
@@ -3,26 +3,22 @@
 # Fast fail the script on failures.
 set -e
 
-dartanalyzer bin/dart_coveralls.dart lib/dart_coveralls.dart
+# Verify that the libraries are error and warning-free.
+echo "Running dartanalyzer..."
+libs=$(find lib -maxdepth 1 -type f -name '*.dart')
+dartanalyzer $DARTANALYZER_FLAGS $libs test/all_tests.dart
 
-dart --checked test/test_all.dart
+# Run the tests.
+echo "Running tests..."
+pub run test:test
 
-# Install dart_coveralls; gather and send coverage data.
-if [ "$COVERALLS_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
-  echo "Running coverage..."
-  dart bin/dart_coveralls.dart report \
+# Gather and send coverage data.
+if [ "$REPO_TOKEN" ] && [ "$TRAVIS_DART_VERSION" = "stable" ]; then
+  echo "Collecting coverage..."
+  pub global activate dart_coveralls
+  pub global run dart_coveralls report \
+    --token $REPO_TOKEN \
     --retry 2 \
     --exclude-test-files \
-    --debug \
-    test/marketData_test.dart
-  echo "Coverage complete."
-else
-  if [ -z ${COVERALLS_TOKEN+x} ]; then echo "COVERALLS_TOKEN is unset"; fi
-  if [ -z ${TRAVIS_DART_VERSION+x} ]; then
-    echo "TRAVIS_DART_VERSION is unset";
-  else
-    echo "TRAVIS_DART_VERSION is $TRAVIS_DART_VERSION";
-  fi
-
-  echo "Skipping coverage for this configuration."
+    test/all_tests.dart
 fi
