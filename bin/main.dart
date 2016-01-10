fabb2e44e4c11c8f6786b5759fd3aea6413220cb
Agustin Berbegall <aberbegall@yahoo.es>
2016-01-10 19:56:51 +0100

    Added missing exit code to terminate the main program

--- bin/main.dart
+++ bin/main.dart
@@ -1,12 +1,16 @@
 // Copyright (c) 2015, Agustin Berbegall. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
+import 'dart:io';
+
+import 'package:dson/dson.dart' as dson;
+
 import '../lib/src/dto/share.dart' as dto;
 import '../lib/src/marketData.dart' as marketStockShares;
 import '../lib/src/repository.dart' as repository;
 
 main() {
-  print('Loading data . . .\n');
+  print('Loading data...');
 
   var listOfShares = new List<String>();
   listOfShares.addAll(['INDEXBME:IB', 'BME:GAM', 'BME:GRF', 'BME:SCYR']);
@@ -16,7 +20,8 @@ main() {
       .getShareDataList(listOfShares)
       .then(storeShareDataList)
       .whenComplete(() {
-    print("All data loaded! :)\n");
+    print("All data loaded!");
+    exit(0);
   }).catchError(onErrorGettingData);
 }
 
@@ -27,8 +32,9 @@ void storeShareDataList(List<dto.share> shares) {
 void storeShare(dto.share share) {
   var redisServer = "localhost:6379/2";
   var redis = new repository.RedisRepository(redisServer);
+  var shareJson = dson.toJson(share);
   redis
-      .setValue(share.name, share.value)
+      .setValue(share.name, shareJson)
       .then((_) => redis.getValue(share.name));
 }
 
